e78148ce439c81294ea4f0dcdd0632e7a1f539e7
Michael Barney, Jr <mbarneyme@gmail.com>
2019-07-30 21:31:48 -0400

    Adds a pylint and twine to pipfile, adds more sane setup.py options

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='TODO',
+    name='awsume-TODO-plugin',
+    version='0.0.0',
     entry_points={
         'awsume': [
             'TODO = TODO'
         ]
     },
+    author='TODO',
+    author_email='TODO',
     py_modules=['TODO'],
 )
